4b8be52f7a224f182013965fd94d553e9c8473fe
Alexander Voronov <43001671+radio-mouse@users.noreply.github.com>
2020-10-05 16:43:08 +0300

    replaced push->return on concat in  largestOfFour

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -83,12 +83,7 @@ largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 85
 largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]) should return [9, 35, 97, 1000000].
 largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]) should return [25, 48, 21, -3].*/
 
-const largestOfFour = arr => {
-    return arr.reduce((acc, el) => {
-        acc.push(Math.max(...el))
-        return acc
-    }, [])
-} 
+const largestOfFour = arr => arr.reduce((acc, el) => acc.concat(Math.max(...el)), [])
 
 
 /*------------------------------------------------------------------------
