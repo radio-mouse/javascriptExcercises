0fb9cf371238b8e4b2e2d32058fc8a76728f8f21
radio-mouse <sanravens@gmail.com>
2020-10-06 11:15:30 +0300

    fixed title

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -220,7 +220,7 @@ const frankenSplice = (arr1, arr2, n) => {
 
 
 /* ------------------------------------------------------------------------
-Basic Algorithm Scripting: Falsy BouncerPassed
+Basic Algorithm Scripting: Falsy Bouncer
 
 Remove all falsy values from an array.
 Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
